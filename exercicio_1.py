e65f1b38946288c6e78c0c1953d00ecc6df2cfb5
laurasoares1214 <163673445+laurasoares1214@users.noreply.github.com>
2024-03-22 10:50:02 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,10 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+
+numero_escolhido = int(input("Digite um número:"))
+if numero_escolhido %2 == 0:
+    print ("Par")
+else: 
+    print ("Ímpar")
+    
