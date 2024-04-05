8996777e4450223b943f5e5c91daeb8e78abab3b
laurasoares1214 <163673445+laurasoares1214@users.noreply.github.com>
2024-04-05 19:14:55 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -86,12 +86,12 @@ print ("Para descobrir se essa variante é relevante digite as seguintes informa
 frequencia_pop = float (input("Digite a frequencia populacional (em porcentagem):"))
 gene_da_variante = input ("Digite o gene:")
 impacto_variante = input ("Digite a Impacto (ALTO ou BAIXO):")
-reads_variante= float (input("Digite os reads:"))
-frequencia_var= float (input("Digite a frequencia alélica (em porcentagem):"))
+reads_variante= float(input("Digite os reads:"))
+frequencia_var= float(input("Digite a frequencia alélica (em porcentagem):"))
 
 artefato = reads_variante <10 or frequencia_var < 20
 condicao1= not artefato and impacto_variante =="ALTO"
-condicao2 = not artefato and (frequencia_pop < 5 or gene_da_variante == "HFE" or gene_da_variante == "GJB2" or gene_da_variante == "MEFV")
+condicao2 = not artefato and (frequencia_pop < 5 and ( gene_da_variante == "HFE" or gene_da_variante == "GJB2" or gene_da_variante == "MEFV"))
 
 if condicao1 or condicao2:
     print (" Resposta: É relevante")
